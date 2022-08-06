2b9faee0d8bc502a2cf40db5c57b47cf544b8590
qphilippot <quentin.philippot@gmail.com>
2022-08-06 17:21:05 +0200

    fix(compiler-interface): typo

--- src/core/interfaces/compiler.interface.ts
+++ src/core/interfaces/compiler.interface.ts
@@ -3,7 +3,7 @@ import CompilerPassInterface from "./compiler-pass.interface";
 
 interface CompilerInterface {
     compile(containerBuilder: ContainerBuilderInterface);
-²    addPass(pass: CompilerPassInterface, step: string, priority?: number);
+    addPass(pass: CompilerPassInterface, step: string, priority?: number);
     getPasses(): CompilerPassInterface[];
     addStep(step: string);
 }
